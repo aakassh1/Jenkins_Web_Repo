d8a4964ef9c5eb49f0a72b9158be9a368dfe77b9
Aakash <aakash4860s@gmail.com>
2025-09-15 17:05:19 +0530

    Modifying existing java file

--- Jenkins_Demo/src/com/jenkins/JenkinsDemo.java
+++ Jenkins_Demo/src/com/jenkins/JenkinsDemo.java
@@ -1,10 +1,14 @@
 package com.jenkins;
 
+import java.util.Date;
+
 public class JenkinsDemo {
 
 	public static void main(String[] args) {
 		System.out.println("This is a demo project for jenkins");
 		System.out.println("Here we are going to run this from jenkins");
+		System.out.println("Now we are making changes in file");
+		System.out.println("Today date is: "+new Date());
 	}
 
 }

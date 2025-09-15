5a3d05cfb288e8bc64d777f8c4061b0205db1222
Aakash <aakash4860s@gmail.com>
2025-09-15 16:25:50 +0530

    Project running from jenkins

---
+++ Jenkins_Demo/src/com/jenkins/JenkinsDemo.java
@@ -0,0 +1,10 @@
+package com.jenkins;
+
+public class JenkinsDemo {
+
+	public static void main(String[] args) {
+		System.out.println("This is a demo project for jenkins");
+		System.out.println("Here we are going to run this from jenkins");
+	}
+
+}
